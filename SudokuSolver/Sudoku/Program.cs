dc566c8a629e9bb4990f8363b25adc2aa6aaa322
Mikey Teal <Mikey.Teal@softwire.com>
2018-01-19 16:06:21 +0000

    fix for borken stuff

---
+++ SudokuSolver/Sudoku/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sudoku
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+
+        public static List<int> createStartConfiguration()
+        {
+            List<int> sudokuPositionList = new List<int>();
+
+            for (int i = 0; i < 81; i++)
+            {
+                sudokuPositionList.Add(0);
+            }
+            sudokuPositionList.Insert(5, 2);
+            sudokuPositionList.Insert(6, 1);
+            sudokuPositionList.Insert(11, 4);
+            sudokuPositionList.Insert(14, 8);
+            sudokuPositionList.Insert(15, 7);
+            sudokuPositionList.Insert(19, 2);
+            sudokuPositionList.Insert(21, 3);
+            sudokuPositionList.Insert(24, 9);
+            sudokuPositionList.Insert(27, 6);
+            sudokuPositionList.Insert(29, 2);
+            sudokuPositionList.Insert(32, 3);
+            sudokuPositionList.Insert(34, 4);
+            sudokuPositionList.Insert(46, 5);
+            sudokuPositionList.Insert(48, 6);
+            sudokuPositionList.Insert(51, 3);
+            sudokuPositionList.Insert(53, 1);
+            sudokuPositionList.Insert(56, 3);
+            sudokuPositionList.Insert(59, 5);
+            sudokuPositionList.Insert(61, 8);
+            sudokuPositionList.Insert(65, 8);
+            sudokuPositionList.Insert(66, 2);
+            sudokuPositionList.Insert(69, 5);
+            sudokuPositionList.Insert(74, 9);
+            sudokuPositionList.Insert(75, 7);
+            return sudokuPositionList;
+        }
+    }
+}
--- WordList/Program.cs
+++ WordList/Program.cs
@@ -9,68 +9,73 @@ namespace WordList
     {
         static void Main(string[] args)
         {
-            var wordList = GetWordList();
-
             Stopwatch sw = new Stopwatch();
 
             Random rnd = new Random();
+            var wordList = GetWordList();
 
             sw.Start();
 
-            for (int i = 0; i < 1000000; i++)
+            for (int i = 0; i < 50000; i++)
             {
                 int rndPosition = rnd.Next(0, wordList.Count);
                 string rndWord = wordList[rndPosition];
 
-                BinarySearchForWord(rndWord, wordList);
+                int wordPosition = BinarySearchForWord(rndWord, wordList);
             }
             sw.Stop();
             Console.WriteLine("Binary search : {0}", sw.Elapsed);
             sw.Reset();
 
             sw.Start();
-            for (int i = 0; i < 1000000; i++)
+
+            for (int i = 0; i < 50000; i++)
             {
                 int rndPosition = rnd.Next(0, wordList.Count);
                 string rndWord = wordList[rndPosition];
 
-                LinearSearchForWord(rndWord, wordList);
+                int wordPosition = LinearSearchForWord(rndWord, wordList);
             }
             sw.Stop();
             Console.WriteLine("Linear search : {0}", sw.Elapsed);
             Console.ReadLine();
         }
 
-        public static void BinarySearchForWord(string s, List<string> list)
+        public static int BinarySearchForWord(string s, List<string> list)
         {
+            var halfwayValue = (int)Math.Floor((double)list.Count / 2);
+            var first = 0;
+            var last = list.Count - 1;
+
             while (true)
             {
-                var halfwayValue = list.Count == 1 ? 0 : (int)Math.Ceiling((double)list.Count / 2) - 1;
-
                 if (s.CompareTo(list[halfwayValue]) == 0)
                 {
-                    break;
+                    return halfwayValue;
                 }
                 if (s.CompareTo(list[halfwayValue]) == -1)
                 {
-                    list.RemoveRange(halfwayValue, list.Count - halfwayValue);
+                    last = halfwayValue - 1;
+                    halfwayValue = (int)Math.Floor((double)(first + last) / 2);
                 }
                 else
                 {
-                    list.RemoveRange(0, halfwayValue+1);
+                    first = halfwayValue + 1;
+                    halfwayValue = (int)Math.Floor((double)(first + last) / 2);
                 }
             }
         }
 
-        public static void LinearSearchForWord(string s, List<string> list)
+        public static int LinearSearchForWord(string s, List<string> list)
         {
-            foreach(var word in list)
+            for(int i = 0; i < list.Count; i++)
             {
-                if(word == s)
+                if(list[i] == s)
                 {
-                    break;
+                    return i;
                 }
             }
+            return 0;
         }
 
         public static List<string> GetWordList()
@@ -89,6 +94,7 @@ namespace WordList
 
                 }
             }
+            wordList.Sort();
             return wordList;
         }
     }
